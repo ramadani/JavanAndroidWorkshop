0560463b45e80646604ad1cb7b2e687faa399f41
Ramadani <email.ramadani@gmail.com>
2017-05-16 14:16:10 +0700

    add price on layout

--- app/src/main/java/id/ramadani/javanworkshop/JustJavaActivity.java
+++ app/src/main/java/id/ramadani/javanworkshop/JustJavaActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import java.text.NumberFormat;
+
 public class JustJavaActivity extends AppCompatActivity {
 
     @Override
@@ -15,11 +17,17 @@ public class JustJavaActivity extends AppCompatActivity {
     }
 
     public void submitOrder(View view) {
-        display(1);
+        display(2);
+        displayPrice(2 * 5);
     }
 
     private void display(int number) {
         TextView tvQuantity = (TextView) findViewById(R.id.tv_quantity);
-        tvQuantity.setText("" + number);
+        tvQuantity.setText(String.valueOf(number));
+    }
+
+    private void displayPrice(int number) {
+        TextView tvPrice = (TextView) findViewById(R.id.tv_price);
+        tvPrice.setText(NumberFormat.getCurrencyInstance().format(number));
     }
 }
