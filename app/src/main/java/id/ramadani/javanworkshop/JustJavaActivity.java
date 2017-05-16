ab54265de8ebf6e8d6f4721198ab445cebc17925
Ramadani <email.ramadani@gmail.com>
2017-05-16 13:52:55 +0700

    add attr onClick on button and setup method it

--- app/src/main/java/id/ramadani/javanworkshop/JustJavaActivity.java
+++ app/src/main/java/id/ramadani/javanworkshop/JustJavaActivity.java
@@ -2,6 +2,8 @@ package id.ramadani.javanworkshop;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class JustJavaActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class JustJavaActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_just_java);
         setTitle(R.string.just_java);
+    }
+
+    public void submitOrder(View view) {
+        display(1);
+    }
+
+    private void display(int number) {
+        TextView tvQuantity = (TextView) findViewById(R.id.tv_quantity);
+        tvQuantity.setText("" + number);
     }
 }
