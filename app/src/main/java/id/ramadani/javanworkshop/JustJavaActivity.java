3f3d2acfe2ba589cffb0ff5f3544e39353e96319
Ramadani <email.ramadani@gmail.com>
2017-05-17 13:03:04 +0700

    increment & descrement with math logic

--- app/src/main/java/id/ramadani/javanworkshop/JustJavaActivity.java
+++ app/src/main/java/id/ramadani/javanworkshop/JustJavaActivity.java
@@ -22,12 +22,14 @@ public class JustJavaActivity extends AppCompatActivity {
     }
 
     public void increment(View view) {
-        int quantity = 3;
+        int quantity = 2;
+        quantity = quantity + 1;
         display(quantity);
     }
 
     public void decrement(View view) {
-        int quantity = 1;
+        int quantity = 2;
+        quantity = quantity - 1;
         display(quantity);
     }
 
